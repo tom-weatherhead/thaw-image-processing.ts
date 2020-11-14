88ff60fe9babfde61259f07fc7afeb8195e29585
Tom Weatherhead <tomw@2hrd4u.org>
2020-11-14 16:08:36 -0700

    Dev

--- babel.config.js
+++ babel.config.js
@@ -17,7 +17,7 @@ const pkg = require('./package.json');
 const supportedNodeVersion = semver.minVersion(pkg.engines.node).version;
 
 module.exports = {
-	'presets': [
+	presets: [
 		[
 			'@babel/preset-env',
 			{
